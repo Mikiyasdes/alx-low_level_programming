040651c91a381b7d7b3f8d17c632b5e79111b4f9
Mikiyas Desalegn <mikiyasdesalegn9@gmail.com>
2022-09-08 16:45:18 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - prints a text
+* main - Prints a text
 *
-* Return Always 0 (Success)
+* Return: Always 0 (success)
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n")
 	return (0);
 }
