661c0a991242f0566aaab1bd04addea1413117f5
Mikiyas Desalegn <mikiyasdesalegn9@gmail.com>
2022-09-08 07:31:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,56 @@
-#!/bin/bash
-#include <stdio.h>
-/**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints *Programming is like building a multilingual puzzule
-* Return: 0
-*/
-int main (void)
-{
-    	printf("with proper grammer, but the outcome is a piece of art,\n");
-	     return (0);
-}
+#include <stdio.h>                                                                                                                    
+
+                                                                                                                                      
+
+/**                                                                                                                                   
+
+                                                                                                                                      
+
+* main - print the string in the put function                                                                                         
+
+                                                                                                                                      
+
+*                                                                                                                                     
+
+                                                                                                                                      
+
+* Description: using the main function                                                                                                
+
+                                                                                                                                      
+
+* this program prints *Programming is like building a multilingual                                                                    
+
+                                                                                                                                      
+
+ puzzule                                                                                                                              
+
+                                                                                                                                      
+
+* Return: 0                                                                                                                           
+
+                                                                                                                                      
+
+*/                                                                                                                                    
+
+                                                                                                                                      
+
+int main (void)                                                                                                                       
+
+                                                                                                                                      
+
+{                                                                                                                                     
+
+                                                                                                                                      
+
+        printf("with proper grammer, but the outcome is a piece of                                                                    
+
+                                                                                                                                      
+
+ art,\n");                                                                                                                            
+
+                                                                                                                                      
+
+             return (0);                                                                                                              
+
+                                                                                                                                      
+
