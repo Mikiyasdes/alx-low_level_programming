0b512edc1994b9708ee65bdd6b14fc31536aa85e
Mikiyas Desalegn <mikiyasdesalegn9@gmail.com>
2022-09-11 10:36:55 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Print random number to variable
+*
+* Return: Always (success)
+*/
+int main(void);
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+	putchar(i);
+}
+putchar('\n');
+return (0);
+}
