ba6d4a6c77888f42ca17f7d0e9ec6aa4c7aac6e4
Mikiyas Desalegn <mikiyasdesalegn9@gmail.com>
2022-09-11 10:11:24 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 /**
-* main - print random number to variable
+* main - Print random number to variable
 *
 * Return: Always (Success)
 */
 int main(void)
-
 {
 char c;
-for (c = 'a'; c <= 'z'; c++);
+for (c = 'a'; c <= 'z'; c++)
 {
-if (c != 'e' && c != 'q')
-{
-	putchar(c);
+	if (c != 'e' && c != 'q')
 
-}
+	{
+		putchar(ch);
+	}
 }
 putchar('\n');
 return (0);
