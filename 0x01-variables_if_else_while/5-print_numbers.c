b4a5abe3d1c2bd24dc54065015713b3d030e6519
Mikiyas Desalegn <mikiyasdesalegn9@gmail.com>
2022-09-11 10:28:32 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Print random number to variable
+*
+* Return: Always (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	printf("%i", i);
+}
+printf("\n");
+return (0);
+}
