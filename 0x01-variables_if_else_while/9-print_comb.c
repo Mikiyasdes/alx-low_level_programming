24cd10153d1332df2f9cb8cb362ca32521150f29
Mikiyas Desalegn <mikiyasdesalegn9@gmail.com>
2022-09-12 08:10:26 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Print numbers
+*
+* Return: Always (Success)
+*/
+int main(void)
+{
+int c;
+for (c = 48; c <= 57; c++)
+{
+	putchar(c);
+	if (c != 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
