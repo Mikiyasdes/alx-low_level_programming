476fe999ef6428803742d5d15a7f16d43bca88ef
Mikiyas Desalegn <mikiyasdesalegn9@gmail.com>
2022-09-15 09:00:39 -0700

    #include <stdio.h>

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,10 +5,10 @@
 */
 int main(void)
 {
-
 int i;
 
 printf("Infinite loop incoming :(\n");
+
 i = 0;
 /*
 *while (i < 10)
@@ -17,5 +17,6 @@ i = 0;
 *}
 */
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
